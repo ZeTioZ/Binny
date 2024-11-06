1ab555e77105e42959bc4781b5b39949ec3c956e
ZeTioZ <donato_gentile@hotmail.it>
2024-11-06 03:46:48 +0100

    Reverting the anti-screen saver (not working on linux)

--- src/main/scala/be/unamur/binny/Binny.scala
+++ src/main/scala/be/unamur/binny/Binny.scala
@@ -7,28 +7,12 @@ import be.unamur.binny.websocket.WebSocket
 import com.phidget22.Hub
 import scalafx.application.JFXApp3
 
-import java.awt.Robot
-import java.util.{Timer, TimerTask}
-
-
 object Binny extends JFXApp3
 {
 	private val system: ActorSystem = ActorSystem("IoTSystem")
 	private val hub: Hub = new Hub()
 	val phidgetHub: ActorRef = system.actorOf(Props(new PhidgetHub(hub)), "phidgetHub")
 	val webSocket: Unit = new WebSocket().start()
-
-	private val robot = new Robot()
-	private val timer = new Timer(true)
-
-	timer.scheduleAtFixedRate(new TimerTask {
-		override def run(): Unit = {
-			val mouseX = java.awt.MouseInfo.getPointerInfo.getLocation.x
-			val mouseY = java.awt.MouseInfo.getPointerInfo.getLocation.y
-			robot.mouseMove(mouseX + 1, mouseY)
-			robot.mouseMove(mouseX, mouseY)
-		}
-	}, 0, 60000)
 
 	override def start(): Unit = {
 		new VirtualAssistantEyes().start()
