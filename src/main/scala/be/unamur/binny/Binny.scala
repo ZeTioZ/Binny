f7da28b14ec46d53f59847814ecff8d0e36d77d5
ZeTioZ <donato_gentile@hotmail.it>
2024-11-06 03:04:06 +0100

    Fixed the app start to be a JFXApp3

--- src/main/scala/be/unamur/binny/Binny.scala
+++ src/main/scala/be/unamur/binny/Binny.scala
@@ -8,16 +8,13 @@ import com.phidget22.Hub
 import scalafx.application.JFXApp3
 
 
-object Binny extends App
+object Binny extends JFXApp3
 {
 	private val system: ActorSystem = ActorSystem("IoTSystem")
 	private val hub = new Hub()
 	val phidgetHub = system.actorOf(Props(new PhidgetHub(hub)), "phidgetHub")
 	val webSocket: Unit = new WebSocket().start()
-	JFXAppLauncher.main(Array())
-}
 
-object JFXAppLauncher extends JFXApp3 {
 	override def start(): Unit = {
 		new be.unamur.binny.animation.VirtualAssistantEyes().start()
 	}
