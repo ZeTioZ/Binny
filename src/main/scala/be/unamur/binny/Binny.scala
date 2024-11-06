1430df1d3c8ba60110d001e91a44c7a276258011
ZeTioZ <donato_gentile@hotmail.it>
2024-11-06 03:09:48 +0100

    Some more typing and import cleanup

--- src/main/scala/be/unamur/binny/Binny.scala
+++ src/main/scala/be/unamur/binny/Binny.scala
@@ -1,6 +1,6 @@
 package be.unamur.binny
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorRef, ActorSystem, Props}
 import be.unamur.binny.actors.PhidgetHub
 import be.unamur.binny.animation.VirtualAssistantEyes
 import be.unamur.binny.websocket.WebSocket
@@ -11,11 +11,11 @@ import scalafx.application.JFXApp3
 object Binny extends JFXApp3
 {
 	private val system: ActorSystem = ActorSystem("IoTSystem")
-	private val hub = new Hub()
-	val phidgetHub = system.actorOf(Props(new PhidgetHub(hub)), "phidgetHub")
+	private val hub: Hub = new Hub()
+	val phidgetHub: ActorRef = system.actorOf(Props(new PhidgetHub(hub)), "phidgetHub")
 	val webSocket: Unit = new WebSocket().start()
 
 	override def start(): Unit = {
-		new be.unamur.binny.animation.VirtualAssistantEyes().start()
+		new VirtualAssistantEyes().start()
 	}
 }
