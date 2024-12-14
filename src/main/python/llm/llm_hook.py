8f49b8942a4f886e4ca60da02184958aca418252
ZeTioZ <donato_gentile@hotmail.it>
2024-12-14 05:25:34 +0100

    First version of Binny with a wake word model trained with OpenWakeWord.

--- src/main/python/llm/llm_hook.py
+++ src/main/python/llm/llm_hook.py
@@ -10,6 +10,10 @@ import cv2
 import os
 import subprocess
 
+import sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+
+from wakeword.wakeword import Wakeword
+
 llm_url = "http://127.0.0.1:1234/v1/chat/completions"
 synthesizer_url = "http://127.0.0.1:8124/synthesize"
 rest_api_url = "http://127.0.0.1:8000"
@@ -22,7 +26,7 @@ def send_request(message, hand_context=None):
 	payload = {
 		"model": "llama-3.2-3b-instruct-uncensored-i1",
 		"messages": [
-			{ "role": "system", "content": f"You are a trash bin named \"Binny\" that always try to be funny and kid friendly. Don't correct the user if he mispronounce your name. The user can ask where to place trash you will have to answer according to these rules: plastic, metal and drink carton into the blue bin, food scraps in green bin, miscellaneous (which can contain paper, packagings, tissues) into black bin and styrofoam, cardboard or glass in none of these bins. The user can also ask you to open the different colored bins. Just answer that you can open the colored bin he wants and give that color as for the bin color chosen. For this session, the user is showing you {hand_context}." },
+			{ "role": "system", "content": f"You are a trash bin named \"Binny\" that always try to be funny and kid friendly. The user can ask where to place trash and you will have to answer according to these rules: plastic, metal and drink carton into the blue bin, food scraps in green bin, miscellaneous (which can contain paper, packagings, tissues) into black bin and styrofoam, cardboard or glass in none of these bins. The user can also ask you to open the different colored bins. Just answer that you can open the colored bin he wants and give that color as for the bin color chosen. If the user doesn't want to trash something respond with the color bin \"none\". For this session, the user is showing you at the camera {hand_context}." },
 			{ "role": "user", "content": message }
 		],
 		"temperature": 0.7,
@@ -74,7 +78,7 @@ def process_command(user_input, hand_context=None):
 	json_structured_response = json.loads(bot_response)
 	sanitize = json_structured_response["message"].replace("\n", " ").replace("\r", "").replace("\t", "").replace("\\", "")
 	sanitize = re.sub(r"\*([^*]+)\*", '', sanitize, flags=re.IGNORECASE)
-	bag_color = json_structured_response["bin_color"]
+	bag_color = json_structured_response["bin_color"] if json_structured_response.get("bin_color") else "none"
 	asyncio.run(websocket.send_ws_message(f"{websocket_url}", f'color:{bag_color.lower()}'))
 	print("Color:", bag_color)
 	print("Sanitized bot response:", sanitize)
@@ -84,64 +88,40 @@ def process_command(user_input, hand_context=None):
 
 async def stt_to_tts():
 	global current_task
-	keyword_variants = ["albany", "minnie", "bunny", "binny", "benny", "beanies", "ben", "benn", "beanie", "ebony", "baby", "binnie", "bin", "binn", "been"]
 	recognizer = sr.Recognizer()
-	try:
-		with sr.Microphone() as source:
-			recognizer.adjust_for_ambient_noise(source, duration=4)
-			while True:
-				try:
-					print("Listening for keyword...")
-					audio1 = recognizer.listen(source)
-					user_input = recognizer.recognize_google(audio1, language="en-US")
-					print(user_input)
-					keyword_found = False
-					for keyword in keyword_variants:
-						if keyword in user_input.lower():
-							keyword_found = True
+	while True:
+		if Wakeword(model_path="wakeword/models/binny_ww_v2.onnx", inference_framework="onnx").run():
+			requests.get(url=f"{rest_api_url}/stop_sound")
+			print("Keyword detected.")
+			if current_task is not None and type(current_task) is threading.Thread:
+				current_task.join()
+				current_task = None
+			await fetch_synthesizer_response(f"{synthesizer_url}/Hey%2C%20what%20can%20I%20do%20for%20you%3F")
+			image_response = await post_image()
+			hand_context = image_response.json().get('message') if image_response is not None else None
+			hand_context = hand_context if hand_context is not None and hand_context in ['e-waste', 'general_waste', 'glass', 'metal', 'organic_waste', 'paper', 'plastic'] else None
+			if hand_context in ['e-waste', 'general_waste', 'glass', 'metal', 'organic_waste', 'paper', 'plastic']:
+				print(f"Hand context recognized: {hand_context}")
+				user_input = f"As you can see, I have a {image_response.json().get('message')} object to throw away. Where should I put it?"
+			else:
+				with sr.Microphone() as source:
+					recognizer.adjust_for_ambient_noise(source, duration=0.5)
+					while True:
+						try:
+							print("Hand context not recognized. Going back to listening for a command...")
+							audio2 = recognizer.listen(source)
+							user_input = recognizer.recognize_google(audio2, language="en-US")
 							break
-				except sr.RequestError as e:
-					print("Could not request results; {0}".format(e))
-				except sr.UnknownValueError as e:
-					print("Unknown error occurred {0}".format(e))
-				except Exception as e:
-					print("An error occurred: ", e)
-				if keyword_found:
-					requests.get(url=f"{rest_api_url}/stop_sound")
-					print("Keyword detected.")
-					if current_task is not None and type(current_task) is threading.Thread:
-						current_task.join()
-						current_task = None
-					await fetch_synthesizer_response(f"{synthesizer_url}/Hey%2C%20what%20can%20I%20do%20for%20you%3F")
-					image_response = await post_image()
-					hand_context = image_response.json().get('message') if image_response is not None else None
-					hand_context = hand_context if hand_context is not None and hand_context in ['e-waste', 'general_waste', 'glass', 'metal', 'organic_waste', 'paper', 'plastic'] else None
-					if hand_context in ['e-waste', 'general_waste', 'glass', 'metal', 'organic_waste', 'paper', 'plastic']:
-						print(f"Hand context recognized: {hand_context}")
-						user_input = f"As you can see, I have a {image_response.json().get('message')} object to throw away. Where should I put it?"
-					else:
-						while True:
-							try:
-								print("Hand context not recognized. Going back to listening for a command...")
-								recognizer.adjust_for_ambient_noise(source, duration=4)
-								recognizer.adjust_for_ambient_noise(source, duration=0.5)
-								audio2 = recognizer.listen(source)
-								user_input = recognizer.recognize_google(audio2, language="en-US")
-								break
-							except sr.RequestError as e:
-								print("Could not request results; {0}".format(e))
-							except sr.UnknownValueError as e:
-								print("Unknown error occurred {0}".format(e))
-							except Exception as e:
-								print("An error occurred: ", e)
-					if user_input is not None:
-						print(user_input)
-						current_task = threading.Thread(target=process_command, args=(user_input, hand_context,))
-						current_task.start()
-	except sr.RequestError as e:
-		print("Could not request results; {0}".format(e))
-	except sr.UnknownValueError as e:
-		print("Unknown error occurred {0}".format(e))
+						except sr.RequestError as e:
+							print("Could not request results; {0}".format(e))
+						except sr.UnknownValueError as e:
+							print("Unknown error occurred {0}".format(e))
+						except Exception as e:
+							print("An error occurred: ", e)
+			if user_input is not None:
+				print(user_input)
+				current_task = threading.Thread(target=process_command, args=(user_input, hand_context,))
+				current_task.start()
 
 
 async def post_image():
