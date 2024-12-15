4e65a51e938a6851a18625122c3f6246ec3a683c
ZeTioZ <donato_gentile@hotmail.it>
2024-12-15 04:29:10 +0100

    Fixed a memory leak issue caused by the wave file not being closed after stopping the sound playback

--- src/main/python/tts-rest-server/utils/playsound.py
+++ src/main/python/tts-rest-server/utils/playsound.py
@@ -8,31 +8,34 @@ class PlaySound:
 		self.audio = pyaudio.PyAudio()
 		self.stream = None
 		self.thread = None
-		self.wf = None
 
 	def play_sound(self, sound_path: str):
 		self.thread = threading.Thread(target=self._play, args=(sound_path,))
 		self.thread.start()
 
 	def _play(self, sound_path: str):
-		self.wf = wave.open(rf"{sound_path}", 'rb')
-
-		# define callback
-		def callback(in_data, frame_count, time_info, status):
-			data = self.wf.readframes(frame_count)
-			return data, pyaudio.paContinue
-
-		# open stream using callback
-		self.stream = self.audio.open(format=self.audio.get_format_from_width(self.wf.getsampwidth()),
-		                              channels=self.wf.getnchannels(),
-		                              rate=self.wf.getframerate(),
-		                              output=True,
-		                              stream_callback=callback)
-		# start the stream
-		self.stream.start_stream()
-		while self.stream is not None and self.stream.is_active():
-			time.sleep(0.1)
-		self.stop_sound()
+		try:
+			with wave.open(rf"{sound_path}", 'rb') as wf:
+				def callback(in_data, frame_count, time_info, status):
+					data = wf.readframes(frame_count)
+					return data, pyaudio.paContinue
+				self.stream = self.audio.open(
+					format=self.audio.get_format_from_width(wf.getsampwidth()),
+					channels=wf.getnchannels(),
+					rate=wf.getframerate(),
+					output=True,
+					stream_callback=callback)
+				self.stream.start_stream()
+				while self.stream is not None and self.stream.is_active():
+					time.sleep(0.1)
+				self.stream.stop_stream()
+				self.stream.close()
+		except Exception as e:
+			print(f"Error while playing sound: {e}")
+			if self.stream is not None:
+				self.stream.stop_stream()
+				self.stream.close()
+				self.stream = None
 
 	def stop_sound(self) -> bool:
 		if self.stream is not None:
